412b2f7181cc065f9ab5a87f2e026ba9f20900f3
Ana <anamaiberg1776@gmail.com>
2026-05-25 22:33:08 -0300

    problemas resolvidos

--- src/main/java/br/edu/ifsc/DVD.java
+++ src/main/java/br/edu/ifsc/DVD.java
@@ -2,9 +2,8 @@ package br.edu.ifsc;
 
 import java.io.Serializable;
 
-public class DVD extends MediaOptica{
+public class DVD extends MediaOptica implements Serializable{
     public DVD(String descricao, String nomeArtista, String nomeAlbum, Integer duracao){
         super(descricao, nomeArtista, nomeAlbum, duracao);
-        Serializable
     }
 }
--- src/main/java/br/edu/ifsc/criacional/builder/Main.java
+++ src/main/java/br/edu/ifsc/criacional/builder/Main.java
@@ -2,7 +2,8 @@ package br.edu.ifsc.criacional.builder;
 
 public class Main {
     public static void main(String[] args) {
-        Usuario usuario = new Usuario().Builder().withName("Joao").withTelefone("99777777").withDataNascimento("10/05/1992").build;
+        Usuario usuario = new Usuario.Builder().withNome("Joao").withTelefone("99777777").withDataNascimento("10/05/1992").withCPF("11111111111").build();
         usuario.print();
+
     }
 }
